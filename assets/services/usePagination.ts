be5f7d600dc11488b58ac3dae1650e29174a0a65
Szymon Uglis <szymon@l7ssha.xyz>
2023-08-07 01:18:20 +0200

    Fix pagination

--- assets/services/usePagination.ts
+++ assets/services/usePagination.ts
@@ -1,11 +1,16 @@
-import { useEffect, useState } from "react";
+import { useMemo, useState } from "react";
 import { PaginatedResponse } from "./ApiService";
+import { GridPaginationModel } from "@mui/x-data-grid/models/gridPaginationProps";
+import { GridCallbackDetails } from "@mui/x-data-grid/models/api";
 
 export interface usePaginationInterface<T> {
   result: T[];
   totalRowCount: number;
   paginationModel: PaginationModel;
-  setPaginationModel: any;
+  setPaginationModel: (
+    model: GridPaginationModel,
+    details: GridCallbackDetails,
+  ) => void;
 }
 
 export interface PaginationModel {
@@ -23,18 +28,29 @@ export function usePagination<T>(
   const [result, setResult] = useState<T[]>([]);
   const [totalRowCount, setTotalRowCount] = useState(0);
   const [paginationModel, setPaginationModel] = useState<PaginationModel>({
-    pageSize: 25,
+    pageSize: 5,
     page: 1,
   });
+  const [previousPaginationModel, setPreviousPaginationModel] =
+    useState<PaginationModel>({ page: -1, pageSize: -1 });
 
-  useEffect(() => {
+  useMemo(() => {
+    // TODO: These two ifs are hacks. Dont know how to fix those
     if (paginationModel.page === 0) {
+      paginationModel.page = 1;
+    }
+
+    if (
+      previousPaginationModel.page === paginationModel.page &&
+      previousPaginationModel.pageSize === paginationModel.pageSize
+    ) {
       return;
     }
 
     callback(paginationModel).then((result) => {
       setTotalRowCount(result.total);
       setResult(result.results);
+      setPreviousPaginationModel(paginationModel);
     });
   }, [paginationModel.page, paginationModel.pageSize]);
 
